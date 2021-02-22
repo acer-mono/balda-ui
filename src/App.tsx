3f0be1ef3e7c313a87ee72b4444a3acc187fc3e4
acer-mono <margaritastar92@gmail.com>
2021-02-22 13:07:24 +0700

    add registration form with bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import './App.css';
-import Board from "./components/Board/Board";
-import { DndProvider } from 'react-dnd'
-import {HTML5Backend} from "react-dnd-html5-backend";
-import Alphabet from "./components/Alphabet/Alphabet";
+import RegistrationView from "./views/RegistrationView";
 
 const App = () => {
   return (
       <div className="App">
-        <DndProvider backend={HTML5Backend}>
-          <Board size={5} />
-          <Alphabet />
-        </DndProvider>
+        <RegistrationView />
       </div>
   );
 }
---
+++ src/views/RegistrationView.tsx
@@ -0,0 +1,98 @@
+import React, {ChangeEvent, useEffect} from 'react';
+import {
+    InputLabel,
+    FormControl,
+    InputAdornment,
+    IconButton,
+    OutlinedInput,
+    Button,
+    Paper
+} from "@material-ui/core";
+import {Visibility, VisibilityOff, SupervisedUserCircle} from "@material-ui/icons";
+import './RegistrationView.css'
+
+const styles = {
+    width: 500
+}
+
+const RegistrationView = () => {
+    const [values, setValues] = React.useState({
+        name: '',
+        password: '',
+        showPassword: false,
+        image: ''
+    });
+
+    const handleClickShowPassword = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        setValues({ ...values, showPassword: !values.showPassword });
+    };
+
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        event.preventDefault();
+    };
+
+    const handleChange = (prop: string) => (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setValues({ ...values, [prop]: event.target.value });
+    };
+
+    useEffect(() => {
+        fetch(`https://source.unsplash.com/random/${styles.width}x${styles.width/2}`).then((response) => {
+            setValues({...values, image: response.url});
+        })
+    }, [values]);
+
+    return (
+        <Paper className="Card" style={{width: `${styles.width}px`}} variant="outlined">
+            <img alt="Random" src={values.image} />
+            <form noValidate autoComplete="off">
+                <FormControl margin="normal" fullWidth variant="outlined">
+                    <InputLabel htmlFor="login">Login</InputLabel>
+                    <OutlinedInput
+                        id="login"
+                        type='text'
+                        value={values.name}
+                        onChange={handleChange('name')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    edge="end">
+                                    <SupervisedUserCircle />
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        labelWidth={70}
+                    />
+                </FormControl>
+                <FormControl margin="normal" fullWidth variant="outlined">
+                    <InputLabel htmlFor="password">Password</InputLabel>
+                    <OutlinedInput
+                        id="password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end">
+                                    {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        labelWidth={70}
+                    />
+                </FormControl>
+                <FormControl margin="normal" fullWidth>
+                    <Button fullWidth type='submit' size="large" variant="contained" color="primary">
+                        Register
+                    </Button>
+                </FormControl>
+            </form>
+        </Paper>
+    );
+}
+
+export default RegistrationView;
